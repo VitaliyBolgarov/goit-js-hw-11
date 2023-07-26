24819e8a324cdb4e2f539b29e7c7559f4fb8188c
VitaliyBolgarov <125477680+VitaliyBolgarov@users.noreply.github.com>
2023-07-26 23:34:12 +0300

    hw-11.1

---
+++ src/fetch.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = 'https://pixabay.com/api';
+export const imagesOnPage = 40;
+
+export async function getImages(currentPage, userInfo) {
+    return await axios.get(`${BASE_URL}/?&page=${currentPage}&q=${userInfo}`, {
+        params: {
+            key: '38442496-05931c79f09bcb64844571ca2',
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            per_page: imagesOnPage,
+        }
+    })
+}
