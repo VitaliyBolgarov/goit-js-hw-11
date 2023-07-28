90c9d37808dba299dedd04e14e9e21d20c038543
VitaliyBolgarov <125477680+VitaliyBolgarov@users.noreply.github.com>
2023-07-28 23:07:24 +0300

    hw-11.4

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ let currentPage = null;
 
 // inputEl.addEventListener('input', checkInput);
 formEl.addEventListener('submit', formSubmit);
-buttonEl.disabled = true;
 loaderEl.hidden = true;
 
 const target = document.querySelector('.js-guard');
@@ -31,7 +30,7 @@ let observer = new IntersectionObserver(onLoad, options);
 function onLoad(entries, observer) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            // currentPage += 1;
+            currentPage += 1;
             getImages(currentPage, userInfo)
                 .then(resp => {
                     const array = resp.data.hits;
@@ -74,31 +73,31 @@ function formSubmit(event) {
         return;
     }
 
-    // inputEl.value = '';
+    inputEl.value = '';
     // buttonEl.disabled = true;
-    // getImages(currentPage, userInfo)
-    //     .then(resp => {
-    //         totalHits = resp.data.totalHits;
-    //         totalPages = Math.ceil(totalHits / imagesOnPage);
-    //         const array = resp.data.hits;
-    //         loaderEl.hidden = true;
-    //         if (array.length === 0) {
-    //             Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
-    //         } else {
-    //             galleryItem.insertAdjacentHTML('beforeend', createMarkup(array));
-    //             lightbox.refresh();
-    //             Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-    //             if (totalHits > imagesOnPage) {
-    //                 observer.observe(target);
-    //             }
-    //         }
-    //     })
-    //     .catch(err => {
-    //         Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    //         loaderEl.hidden = true;
-    //         console.log(err);
-    //     }
-    //     );
+    getImages(currentPage, userInfo)
+        .then(resp => {
+            totalHits = resp.data.totalHits;
+            totalPages = Math.ceil(totalHits / imagesOnPage);
+            const array = resp.data.hits;
+            loaderEl.hidden = true;
+            if (array.length === 0) {
+                Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+            } else {
+                galleryItem.insertAdjacentHTML('beforeend', createMarkup(array));
+                lightbox.refresh();
+                Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+                if (totalHits > imagesOnPage) {
+                    observer.observe(target);
+                }
+            }
+        })
+        .catch(err => {
+            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+            loaderEl.hidden = true;
+            console.log(err);
+        }
+        );
 }
 
 function createMarkup(array) {
